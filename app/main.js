db7062d06d4658cdd7b66dd2b3f7c1992396970e
crnisamuraj <m1ivancevic@gmail.com>
2019-10-31 00:05:14 +0100

    append data to file

--- app/main.js
+++ app/main.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
 var speedtest = require('speedtest-net');
 var test = speedtest({maxTime: 5000});
 
+const logPath = './app/logs/log.txt';
+
+var write = function (path, data) {
+	try {
+		if (fs.existsSync(path)){
+			//console.dir('exists');
+			fs.appendFile(path, `${JSON.stringify(data)},` , (err) => {
+				if (err) {
+					return console.dir(err);
+				}
+			});
+		}
+	} catch (error) {
+		console.dir(error);
+	};
+}
+
 test.on('data', data => {
 	console.dir(data);
+	write(logPath, data);
+	
 });
 
 test.on('error', err => {
