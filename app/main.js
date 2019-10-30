3c8b3f072cb9bd6b9212167431a5f08ba608b57d
crnisamuraj <m1ivancevic@gmail.com>
2019-10-31 00:22:22 +0100

    write new file if logfile doesnt exist

--- app/main.js
+++ app/main.js
@@ -3,32 +3,42 @@ const os = require('os');
 var speedtest = require('speedtest-net');
 var test = speedtest({maxTime: 5000});
 
-const logPath = './app/logs/log.txt';
+const logPath = './app/speedLog/log.log';
 
 /**
  * TODO
  * ADD COMMANDER TO PARSE CLI ARGUMENTS FOR PATH
 **/
 
+var append = function (path, data) {
+	fs.appendFile(path, `${JSON.stringify(data)},${os.EOL}` , (err) => {
+		if (err) {
+			return console.dir(err);
+		}
+	});
+}
+
 var write = function (path, data) {
+	fs.writeFile(path, `${JSON.stringify(data)},${os.EOL}` , (err) => {
+		if (err) {
+			return console.dir(err);
+		}
+	});
+}
+
+test.on('data', data => {
+	console.dir(data);	
 	try {
-		if (fs.existsSync(path)){
-			//console.dir('exists');
-			fs.appendFile(path, `${os.EOL}${JSON.stringify(data)},` , (err) => {
-				if (err) {
-					return console.dir(err);
-				}
-			});
+		if (fs.existsSync(logPath)){
+			console.dir('log file exists');
+			append(logPath, data);
+		} else {
+			console.dir(`log file doesn't exists`);
+			write(logPath, data);
 		}
 	} catch (error) {
 		console.dir(error);
 	};
-}
-
-test.on('data', data => {
-	console.dir(data);
-	write(logPath, data);
-	
 });
 
 test.on('error', err => {
